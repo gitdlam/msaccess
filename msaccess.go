b2464c31d710bf1a89bb6f222853e81d1ebee3f5
gitdlam <gitdlam@users.noreply.github.com>
2019-02-20 12:26:47 +1100

    snippet

---
+++ msaccess.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"github.com/go-ole/go-ole"
+	"github.com/go-ole/go-ole/oleutil"
+	//	"log"
+	//	"strconv"
+	"github.com/AllenDang/w32"
+	"strings"
+	"time"
+)
+
+func run_file(msaccess_file string, run_sub string, arg string) error {
+
+	//fmt.Printf("%s",reflect.TypeOf(access))
+	//
+
+	start := time.Now()
+	global.mutex.Lock()
+	global.mutex_lock_started = time.Now()
+	args := strings.Fields(arg)
+
+	ole.CoInitialize(0)
+
+	//unknown, _ := oleutil.CreateObject("Access.Application")
+	clsid, _ := ole.CLSIDFromString("{73A4C9C1-D68D-11D0-98BF-00A0C90DC8D9}")
+	unknown, _ := ole.CreateInstance(clsid, ole.IID_IUnknown)
+	access, _ := unknown.QueryInterface(ole.IID_IDispatch)
+	//w32.GetWindowThreadProcessId(
+
+	//	global.access_ip = strconv.FormatInt((oleutil.MustCallMethod(access, "SysCmd", 609).Val), 10)
+
+	//	log.Printf("ip %s\n", global.access_ip)
+
+	global.nats_c.Publish("log", "g305transport Before OpenCurrentDatabase")
+
+	r, err := oleutil.CallMethod(access, "OpenCurrentDatabase", msaccess_file, true)
+	if err != nil {
+		global.nats_c.Publish("log", "g305transport "+err.Error())
+		w32.PostMessage(w32.HWND(oleutil.MustCallMethod(access, "hWndAccessApp").Val), w32.WM_QUIT, 0, 0)
+		time.Sleep(time.Duration(1) * time.Second)
+		oleutil.CallMethod(access, "Quit", 2)
+		access.Release()
+		panic(err.Error())
+	}
+
+	r.ToIDispatch()
+
+	//	oleutil.MustCallMethod(access, "OpenCurrentDatabase", msaccess_file, true).ToIDispatch()
+
+	global.nats_c.Publish("log", "g305transport After OpenCurrentDatabase")
+
+	switch len(args) {
+	case 0:
+		oleutil.MustCallMethod(access, "Run", run_sub).ToIDispatch()
+	case 1:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0]).ToIDispatch()
+	case 2:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0], args[1]).ToIDispatch()
+	case 3:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0], args[1], args[2]).ToIDispatch()
+	case 4:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0], args[1], args[2], args[3]).ToIDispatch()
+	case 5:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0], args[1], args[2], args[3], args[4]).ToIDispatch()
+	case 6:
+		oleutil.MustCallMethod(access, "Run", run_sub, args[0], args[1], args[2], args[3], args[4], args[5]).ToIDispatch()
+
+	}
+
+	oleutil.CallMethod(access, "CloseCurrentDatabase")
+	//go log_tran_update(90, "msaccess")
+
+	elapsed := time.Since(start)
+
+	//w32 was needed because somehow the "local system" account won't quit the application.
+	w32.PostMessage(w32.HWND(oleutil.MustCallMethod(access, "hWndAccessApp").Val), w32.WM_QUIT, 0, 0)
+	// sleep was mentioned on the net to give PostMessage time
+	time.Sleep(time.Duration(1) * time.Second)
+	oleutil.CallMethod(access, "Quit", 2)
+
+	access.Release()
+	//	time.Sleep(2000000000)
+	//	ole.CoUninitialize()
+	global.mutex.Unlock()
+	fmt.Printf("\nvia OLE took %s\n", elapsed)
+
+	return nil
+
+}
